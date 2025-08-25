c5faa8378a795c06a50e579f28a99b33b9f74b04
MaazHusyan <mazhusyan120@gmail.com>
2025-08-25 16:21:07 +0500

    edit the code

--- simple_test_agent.py
+++ simple_test_agent.py
@@ -1,32 +1,34 @@
 from agents import Agent, Runner, SQLiteSession, function_tool, ModelSettings
 
-from pydantic import BaseModel
+# from pydantic import BaseModel
 import asyncio
 
 from gemini_model import geminiModel, config
 
-@function_tool
-def tellName(name: str):
-    """Return User name from chat"""
-    return {"name": name}
 
 @function_tool
 def tellAge(age: int):
     """Return User Age from chat"""
     return {"age": age}
 
-class getStructuredOutput(BaseModel):
-    name: str |None = None
-    age: str |None = None
+@function_tool
+def tellName(name: str):
+    """Return User name from chat"""
+    return {"name": name}
+
+# class getStructuredOutput(BaseModel):
+#     name: str |None = None
+#     age: str |None = None
 
 
 myAgent = Agent(
 name="assistant",
 model=geminiModel,
 instructions="""You are a help full assistant. Use tools to get information""",
-tools=[tellName, tellAge],
-model_settings=ModelSettings(tool_choice="auto"),
-output_type=getStructuredOutput
+tools=[tellAge, tellName],
+tool_use_behavior="stop_on_first_tool",
+model_settings=ModelSettings(tool_choice="required"),
+# output_type=getStructuredOutput
 )
 
 async def runner():
@@ -45,7 +47,8 @@ async def runner():
         input=userInput,
         session=session,
         run_config=config,
-        max_turns=10
+        max_turns=10,
+        
         )
                 
         print(result.final_output)
